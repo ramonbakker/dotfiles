303cda7c8945dad9ae10161c675500d7398ab4a2
Ramon Bakker <ramon.b@live.nl>
2021-04-29 08:37:18 +0200

    View details for current Timewarrior entry

--- tmux/timew-elapsed-day.sh
+++ tmux/timew-elapsed-day.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# Script for Timewarrior that outputs the logged time for today.
+# Script for Timewarrior that outputs the logged time for today and the active entry.
 # The output can be used for the tmux status line.
 TIMEW_EXPORT=`timew export :day`
 TIMEW_DAY_TOTAL=0
 
 DAY_ENTRY_NUMBER=0
 CURRENT_DAY_ENTRY=`echo $TIMEW_EXPORT | jq --argjson day_entry_number $DAY_ENTRY_NUMBER '.[$day_entry_number]'`
+CURRENT_DAY_ENTRY_TAG=''
+
 while [ "$CURRENT_DAY_ENTRY" != null ]
 do
     TIMEW_DAY_START=`echo $CURRENT_DAY_ENTRY | jq ".start" | sed -e 's/"//g'`
@@ -16,8 +18,10 @@ do
         TIMEW_DAY_END="now"
     fi
 
-    CURRENT_DAY_ENTRY_DIFF=`/usr/bin/dateutils.ddiff -i '%Y%m%dT%H%M%SZ' -f '%S' $TIMEW_DAY_START $TIMEW_DAY_END`
+    CURRENT_DAY_ENTRY_DIFF=`/usr/local/bin/datediff -i '%Y%m%dT%H%M%SZ' -f '%S' $TIMEW_DAY_START $TIMEW_DAY_END`
     TIMEW_DAY_TOTAL=$(($TIMEW_DAY_TOTAL + $CURRENT_DAY_ENTRY_DIFF))
+
+    CURRENT_DAY_ENTRY_TAG=`echo $TIMEW_EXPORT | jq --argjson day_entry_number $DAY_ENTRY_NUMBER '.[$day_entry_number].tags[0]'`
 
     DAY_ENTRY_NUMBER=$(($DAY_ENTRY_NUMBER + 1))
     CURRENT_DAY_ENTRY=`echo $TIMEW_EXPORT | jq --argjson day_entry_number $DAY_ENTRY_NUMBER '.[$day_entry_number]'`
@@ -28,4 +32,14 @@ SECONDS_IN_HOUR=3600
 TIMEW_DAY_HOURS=`printf "%02.0f" $(($TIMEW_DAY_TOTAL / $SECONDS_IN_HOUR))`
 TIMEW_DAY_MINUTES=`printf "%02.0f" $(($TIMEW_DAY_TOTAL % $SECONDS_IN_HOUR / 60))`
 TIMEW_DAY_SECONDS=`printf "%02.0f" $(($TIMEW_DAY_TOTAL % $SECONDS_IN_HOUR % 60))`
-echo $TIMEW_DAY_HOURS:$TIMEW_DAY_MINUTES:$TIMEW_DAY_SECONDS
+
+TIMEW_CURRENT_HOURS=`printf "%02.0f" $(($CURRENT_DAY_ENTRY_DIFF / $SECONDS_IN_HOUR))`
+TIMEW_CURRENT_MINUTES=`printf "%02.0f" $(($CURRENT_DAY_ENTRY_DIFF % $SECONDS_IN_HOUR / 60))`
+TIMEW_CURRENT_SECONDS=`printf "%02.0f" $(($CURRENT_DAY_ENTRY_DIFF % $SECONDS_IN_HOUR % 60))`
+
+if [ $CURRENT_DAY_ENTRY_TAG == null ]
+then
+    CURRENT_DAY_ENTRY_TAG="N/A"
+fi
+
+echo "$TIMEW_DAY_HOURS:$TIMEW_DAY_MINUTES:$TIMEW_DAY_SECONDS [$TIMEW_CURRENT_HOURS:$TIMEW_CURRENT_MINUTES:$TIMEW_CURRENT_SECONDS $CURRENT_DAY_ENTRY_TAG]"
