041c6ac57507bd1f918f198e5602b06b95814da4
Ramon Bakker <ramon.b@live.nl>
2021-04-30 08:23:22 +0200

    Specify full path to used binaries
    This makes the script usable for tools like MTMR.

--- tmux/timew-elapsed-day.sh
+++ tmux/timew-elapsed-day.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 # Script for Timewarrior that outputs the logged time for today and the active entry.
 # The output can be used for the tmux status line.
-TIMEW_EXPORT=`timew export :day`
+TIMEW_BIN="/usr/local/bin/timew"
+JQ_BIN="/usr/local/bin/jq"
+DATEDIFF_BIN="/usr/local/bin/datediff"
+
+TIMEW_EXPORT=`$TIMEW_BIN export :day`
 TIMEW_DAY_TOTAL=0
 
 DAY_ENTRY_NUMBER=0
-CURRENT_DAY_ENTRY=`echo $TIMEW_EXPORT | jq --argjson day_entry_number $DAY_ENTRY_NUMBER '.[$day_entry_number]'`
+CURRENT_DAY_ENTRY=`echo $TIMEW_EXPORT | $JQ_BIN --argjson day_entry_number $DAY_ENTRY_NUMBER '.[$day_entry_number]'`
 CURRENT_DAY_ENTRY_TAG=''
 
 while [ "$CURRENT_DAY_ENTRY" != null ]
 do
-    TIMEW_DAY_START=`echo $CURRENT_DAY_ENTRY | jq ".start" | sed -e 's/"//g'`
-    TIMEW_DAY_END=`echo $CURRENT_DAY_ENTRY | jq ".end" | sed -e 's/"//g'`
+    TIMEW_DAY_START=`echo $CURRENT_DAY_ENTRY | $JQ_BIN ".start" | sed -e 's/"//g'`
+    TIMEW_DAY_END=`echo $CURRENT_DAY_ENTRY | $JQ_BIN ".end" | sed -e 's/"//g'`
 
     if [ $TIMEW_DAY_END == null ]
     then
         TIMEW_DAY_END="now"
     fi
 
-    CURRENT_DAY_ENTRY_DIFF=`/usr/local/bin/datediff -i '%Y%m%dT%H%M%SZ' -f '%S' $TIMEW_DAY_START $TIMEW_DAY_END`
+    CURRENT_DAY_ENTRY_DIFF=`$DATEDIFF_BIN -i '%Y%m%dT%H%M%SZ' -f '%S' $TIMEW_DAY_START $TIMEW_DAY_END`
     TIMEW_DAY_TOTAL=$(($TIMEW_DAY_TOTAL + $CURRENT_DAY_ENTRY_DIFF))
 
-    CURRENT_DAY_ENTRY_TAG=`echo $TIMEW_EXPORT | jq --argjson day_entry_number $DAY_ENTRY_NUMBER '.[$day_entry_number].tags[0]'`
+    CURRENT_DAY_ENTRY_TAG=`echo $TIMEW_EXPORT | $JQ_BIN --argjson day_entry_number $DAY_ENTRY_NUMBER '.[$day_entry_number].tags[0]'`
 
     DAY_ENTRY_NUMBER=$(($DAY_ENTRY_NUMBER + 1))
-    CURRENT_DAY_ENTRY=`echo $TIMEW_EXPORT | jq --argjson day_entry_number $DAY_ENTRY_NUMBER '.[$day_entry_number]'`
+    CURRENT_DAY_ENTRY=`echo $TIMEW_EXPORT | $JQ_BIN --argjson day_entry_number $DAY_ENTRY_NUMBER '.[$day_entry_number]'`
 done
 
 SECONDS_IN_HOUR=3600
@@ -37,7 +41,7 @@ TIMEW_CURRENT_HOURS=`printf "%02.0f" $(($CURRENT_DAY_ENTRY_DIFF / $SECONDS_IN_HO
 TIMEW_CURRENT_MINUTES=`printf "%02.0f" $(($CURRENT_DAY_ENTRY_DIFF % $SECONDS_IN_HOUR / 60))`
 TIMEW_CURRENT_SECONDS=`printf "%02.0f" $(($CURRENT_DAY_ENTRY_DIFF % $SECONDS_IN_HOUR % 60))`
 
-if [ $CURRENT_DAY_ENTRY_TAG == null ]
+if [ "$CURRENT_DAY_ENTRY_TAG" == null ]
 then
     CURRENT_DAY_ENTRY_TAG="N/A"
 fi
