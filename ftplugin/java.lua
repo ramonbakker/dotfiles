3cc3ee1501054e72710c6f4da92bbce628dd4389
Ramon Bakker <rbakker@ilionx.com>
2023-10-12 15:15:34 +0200

    Add configuration for JDT LS

---
+++ ftplugin/java.lua
@@ -0,0 +1,54 @@
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  buf_set_keymap('n', '<Leader>oi', '<Cmd>lua require"jdtls".organize_imports()<CR>', opts)
+
+  require('jdtls').setup_dap({ hotcodereplace = 'auto' })
+  require('jdtls.dap').setup_dap_main_class_configs()
+
+  vim.lsp.inlay_hint(bufnr, true)
+end
+
+local bundles = {
+  vim.fn.glob(os.getenv("HOME") .. '/java/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar', 1),
+}
+
+vim.list_extend(bundles, vim.split(vim.fn.glob(os.getenv("HOME") .. '/java/vscode-java-test/server/*.jar', 1), "\n"))
+
+local jdtls = require('jdtls')
+
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+extendedClientCapabilities.onCompletionItemSelectedCommand = "editor.action.triggerParameterHints"
+extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = os.getenv("HOME") .. '/workspaces/' .. project_name
+
+local config = {
+    cmd = {
+        os.getenv("HOME") .. '/.local/bin/jdtls',
+        '--jvm-arg=-javaagent:' .. os.getenv("HOME") .. '/.m2/repository/org/projectlombok/lombok/1.18.26/lombok-1.18.26.jar',
+        '-data', workspace_dir,
+    },
+    init_options = {
+      bundles = bundles;
+      extendedClientCapabilities = extendedClientCapabilities
+    },
+    filetypes = {
+      "java",
+    },
+    settings = {
+      java = {
+        inlayHints = { parameterNames = { enabled = "all" } }
+      }
+    },
+    on_attach = on_attach
+}
+
+require('jdtls').start_or_attach(config)
--- lua/commands.lua
+++ lua/commands.lua
@@ -23,3 +23,26 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     pattern = '*',
     command = 'silent! lua vim.highlight.on_yank { timeout = 100 }',
 })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        local opts = { buffer = ev.buf }
+
+        vim.keymap.set('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+        vim.keymap.set('n', 'gd', '<Cmd>Telescope lsp_definitions<CR>', opts)
+        vim.keymap.set('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+        vim.keymap.set('n', '<Leader>i', '<Cmd>Telescope lsp_implementations<CR>', opts)
+        vim.keymap.set('n', '<Leader>k', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+        vim.keymap.set('n', '<Leader>wa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+        vim.keymap.set('n', '<Leader>wr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+        vim.keymap.set('n', '<Leader>wl', '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+        vim.keymap.set('n', '<Leader>D', '<Cmd>Telescope lsp_type_definitions<CR>', opts)
+        vim.keymap.set('n', '<Leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+        vim.keymap.set('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+        vim.keymap.set('n', '<Leader>e', '<Cmd>lua vim.diagnostic.open_float()<CR>', opts)
+        vim.keymap.set('n', '[d', '<Cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+        vim.keymap.set('n', ']d', '<Cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+        vim.keymap.set('n', '<Leader>q', '<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    end
+})
