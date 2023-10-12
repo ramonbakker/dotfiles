76be0923e51042a916221db6a327411868271da3
Ramon Bakker <rbakker@ilionx.com>
2023-10-12 14:27:28 +0200

    Fix command 'silent' not found error

--- lua/commands.lua
+++ lua/commands.lua
@@ -2,21 +2,21 @@
 -- Install php-cs-fixer with 'composer global require friendsofphp/php-cs-fixer'
 vim.api.nvim_create_autocmd('BufWritePost', {
     pattern = '*.php',
-    command = '!silent call PhpCsFixerFixFile()',
+    command = 'silent! call PhpCsFixerFixFile()',
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
     pattern = { '*.php', '*.java' },
-    command = '!silent lua vim.lsp.buf.formatting_sync()',
+    command = 'silent! lua vim.lsp.buf.formatting_sync()',
 })
 vim.api.nvim_create_autocmd('BufWritePre', {
     pattern = { '*.ts', '*.html', '*.scss', '*.css', '*.json' },
-    command = '!silent Prettier',
+    command = 'silent! Prettier',
 })
 
 vim.api.nvim_create_autocmd('User', {
     pattern = 'TelescopePreviewerLoaded',
-    command = '!silent setlocal wrap',
+    command = 'silent! setlocal wrap',
 })
 
 vim.api.nvim_create_autocmd('TextYankPost', {
