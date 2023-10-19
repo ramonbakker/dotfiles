47bdb82a0306b5b129694bd0bf051ef1b4bfada5
Ramon Bakker <ramon.b@live.nl>
2023-10-19 14:02:10 +0200

    Remove Syntastic configuration
    Syntastic is no longer used.

--- nvim/.config/nvim/after/plugin/syntastic.lua
+++
@@ -1,6 +0,0 @@
-vim.opt.statusline:append('%#warningmsg#%{SyntasticStatuslineFlag()}%*')
-
-vim.g.syntastic_always_populate_loc_list = 1
-vim.g.syntastic_auto_loc_list = 1
-vim.g.syntastic_check_on_open = 1
-vim.g.syntastic_check_on_wq = 0
