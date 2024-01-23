99e0baf078946eb02af156040062fe4422561f5d
Ramon Bakker <ramon.b@live.nl>
2024-01-23 15:39:41 +0100

    Add configuration for WezTerm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+config.enable_tab_bar = false
+config.font = wezterm.font('Hack')
+config.font_size = 14
+config.colors = {
+    foreground = '#00ff00',
+    background = '#1f1f1f',
+    cursor_bg = 'white'
+}
+config.default_cursor_style = 'SteadyBar'
+config.window_background_opacity = 0.9
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0
+}
+
+return config
