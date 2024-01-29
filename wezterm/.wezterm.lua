4dfcdd488d7ac0114d033324fe19be6630100b34
Ramon Bakker <ramon.b@live.nl>
2024-01-29 13:58:50 +0100

    Set fallback font for icons

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,10 @@ local config = {}
 
 config.front_end = 'WebGpu'
 config.enable_tab_bar = false
-config.font = wezterm.font('Hack')
+config.font = wezterm.font_with_fallback {
+    'Hack',
+    'Hack Nerd Font'
+}
 config.font_size = 14
 config.colors = {
     foreground = '#00ff00',
