542d855bd07c8b2c1741b1e2512ee72be6e82322
Ramon Bakker <ramon.bakker@vodafoneziggo.com>
2024-03-14 10:11:02 +0100

    Fix conflicting package name

--- nvim/.config/nvim/lua/plugins/file-management.lua
+++ nvim/.config/nvim/lua/plugins/file-management.lua
@@ -5,7 +5,10 @@ return {
             'nvim-lua/popup.nvim',
             'nvim-lua/plenary.nvim',
             'nvim-telescope/telescope-fzy-native.nvim',
-            'kyazdani42/nvim-web-devicons',
+            {
+                'kyazdani42/nvim-web-devicons',
+                name = 'kyazdani42_nvim-web-devicons'
+            },
             'nvim-telescope/telescope-media-files.nvim',
             'nvim-telescope/telescope-ui-select.nvim'
         }
