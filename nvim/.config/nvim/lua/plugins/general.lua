73d0458fe3fa7669436a3beb093ff1e3cf78c049
Ramon Bakker <ramon.b@live.nl>
2025-01-08 09:49:21 +0100

    Remove Noice
    LSP hover/signature help now uses Treesitter for highlighting of
    Markdown content, so Noice isn't needed anymore.

--- nvim/.config/nvim/lua/plugins/general.lua
+++ nvim/.config/nvim/lua/plugins/general.lua
@@ -1,10 +1,3 @@
 return {
     'christoomey/vim-tmux-navigator',
-    {
-        'folke/noice.nvim',
-        event = 'VeryLazy',
-        dependencies = {
-            'MunifTanjim/nui.nvim',
-        }
-    },
 }
