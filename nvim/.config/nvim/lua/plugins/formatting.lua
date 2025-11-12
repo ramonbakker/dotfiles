77332f8a6bf606a5c821f2c83e6afb79075d9b57
Ramon Bakker <9799499+ramonbakker@users.noreply.github.com>
2025-11-12 15:19:53 +0100

    Use original conform.nvim version again
    https://github.com/stevearc/conform.nvim/pull/695 has been merged.

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -3,10 +3,5 @@ return {
         'prettier/vim-prettier',
         build = 'yarn install'
     },
-    -- see https://github.com/stevearc/conform.nvim/issues/699
-    -- 'stevearc/conform.nvim'
-    {
-        'vincentlabelle/conform.nvim',
-        branch = 'fix-csharpier'
-    }
+    'stevearc/conform.nvim'
 }
