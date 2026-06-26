dc86c09fda60d5811146f4f3397be9a7e29990c0
Ramon Bakker <9799499+ramonbakker@users.noreply.github.com>
2026-06-26 13:26:58 +0200

    Remove todo-comments
    Remove because of runtime errors.

--- nvim/.config/nvim/lua/plugins/utilities.lua
+++ nvim/.config/nvim/lua/plugins/utilities.lua
@@ -2,12 +2,6 @@ return {
     'kylechui/nvim-surround',
     'mbbill/undotree',
     'windwp/nvim-autopairs',
-    {
-        'folke/todo-comments.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim'
-        }
-    },
     'andymass/vim-matchup',
     {
         'rest-nvim/rest.nvim',
